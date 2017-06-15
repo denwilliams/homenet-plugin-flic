f11cd8f936b41a4e40e46a720436421869be02ee
Den Williams <work@denwilliams.net>
2017-06-15 19:03:16 +1000

    update to new plugin loader

--- src/loader.ts
+++ src/loader.ts
@@ -1,53 +1,58 @@
-import { plugin, service, Dict, IPluginLoader, ILogger, IConfig, IButtonManager, IButton } from '@homenet/core';
+import { Dict, IPluginLoader, ILogger, IConfig, IButtonManager, IButton } from '@homenet/core';
 import { FlicClient } from './client';
 
-@plugin()
-export class FlicPluginLoader implements IPluginLoader {
-  private _logger : ILogger;
-  private _config : IConfig;
-  private _controllers : Dict<FlicClient>;
+export function create(annotate): { FlicPluginLoader: new(...args: any[]) => IPluginLoader } {
 
-  constructor(
-          @service('IButtonManager') buttons: IButtonManager,
-          @service('IConfig') config: IConfig,
-          @service('ILogger') logger: ILogger) {
+  @annotate.plugin()
+  class FlicPluginLoader implements IPluginLoader {
+    private _logger : ILogger;
+    private _config : IConfig;
+    private _controllers : Dict<FlicClient>;
 
-    this._logger = logger;
-    this._config = config;
+    constructor(
+            @annotate.service('IButtonManager') buttons: IButtonManager,
+            @annotate.service('IConfig') config: IConfig,
+            @annotate.service('ILogger') logger: ILogger) {
 
-    this._init();
+      this._logger = logger;
+      this._config = config;
 
-    buttons.addType('flic', this._createFactory());
-  }
+      this._init();
 
-  load() : void {
-    this._logger.info('Connecting to Flic servers...');
-    Object.keys(this._controllers).forEach(key => {
-      this._controllers[key].start();
-    });
-  }
+      buttons.addType('flic', this._createFactory());
+    }
 
-  private _init() : void {
-    this._logger.info('Starting Flic plugin');
-
-    const flicConfig = (<any>this._config).flic || {};
-    const serversConfigs = flicConfig.servers || [];
-
-    this._controllers = {};
-    serversConfigs.forEach(c => {
-      const client = new FlicClient(c.id, c.host, c.port);
-      this._controllers[c.id] = client;
-      client.on('error', (err) => {
-        this._logger.warn(`Unable to connect to Flic daemon: ${err.message}`);
+    load() : void {
+      this._logger.info('Connecting to Flic servers...');
+      Object.keys(this._controllers).forEach(key => {
+        this._controllers[key].start();
       });
-    });
+    }
+
+    private _init() : void {
+      this._logger.info('Starting Flic plugin');
+
+      const flicConfig = (<any>this._config).flic || {};
+      const serversConfigs = flicConfig.servers || [];
+
+      this._controllers = {};
+      serversConfigs.forEach(c => {
+        const client = new FlicClient(c.id, c.host, c.port);
+        this._controllers[c.id] = client;
+        client.on('error', (err) => {
+          this._logger.warn(`Unable to connect to Flic daemon: ${err.message}`);
+        });
+      });
+    }
+
+    private _createFactory() {
+      return (id: string, opts: {address: string, controller: string}) : IButton => {
+        this._logger.info(`Adding Flic button: ${id}`);
+        const client: FlicClient = this._controllers[opts.controller];
+        return client.getButton(id, opts.address);
+      };
+    }
   }
 
-  private _createFactory() {
-    return (id: string, opts: {address: string, controller: string}) : IButton => {
-      this._logger.info(`Adding Flic button: ${id}`);
-      const client: FlicClient = this._controllers[opts.controller];
-      return client.getButton(id, opts.address);
-    };
-  }
+  return { FlicPluginLoader };
 }
